50e5c2c9dbcfb3244672222ab6711e05eef27ac2
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-07-13 05:49:29 +0700

    Create stack.py

---
+++ data_structure/stacks/stack.py
@@ -0,0 +1,17 @@
+# membuat wadah awal
+my_stack = []
+
+# gunakan fungsi append untuk push ke dalam stack
+my_stack.append(1)
+my_stack.append(2)
+my_stack.append(3)
+my_stack.append(4)
+
+print(my_stack)
+# [1, 2, 3, 4]
+
+# gunakan fungsi pop untuk mengambil datanya 
+my_stack.pop()
+my_stack.pop()
+print(my_stack)
+# [1, 2]
